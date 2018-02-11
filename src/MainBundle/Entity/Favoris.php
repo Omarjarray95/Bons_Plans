e5ef3f25991addd6d07ea79390783cfc9d4d80e1
Ons Ben Othmen <ons.benothmen@esprit.tn>
2018-02-11 15:00:47 +0100

    Entities2

--- src/MainBundle/Entity/Favoris.php
+++ src/MainBundle/Entity/Favoris.php
@@ -20,7 +20,48 @@ class Favoris
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
+    /**
+     * @ORM\ManyToOne(targetEntity="MainBundle\Entity\User")
+     * @ORM\JoinColumn(name="id_user",referencedColumnName="id")
+     */
+    private $id_user;
+    /**
+     * @ORM\ManyToOne(targetEntity="MainBundle\Entity\Etablissement")
+     * @ORM\JoinColumn(name="id_etablissement",referencedColumnName="id")
+     */
+    private $id_etablissement;
 
+    /**
+     * @return mixed
+     */
+    public function getIdUser()
+    {
+        return $this->id_user;
+    }
+
+    /**
+     * @param mixed $id_user
+     */
+    public function setIdUser($id_user)
+    {
+        $this->id_user = $id_user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdEtablissement()
+    {
+        return $this->id_etablissement;
+    }
+
+    /**
+     * @param mixed $id_etablissement
+     */
+    public function setIdEtablissement($id_etablissement)
+    {
+        $this->id_etablissement = $id_etablissement;
+    }
 
     /**
      * Get id
@@ -32,4 +73,3 @@ class Favoris
         return $this->id;
     }
 }
-
--- src/MainBundle/Entity/Service.php
+++ src/MainBundle/Entity/Service.php
@@ -21,7 +21,26 @@ class Service
      */
     private $id;
 
+    /**
+     * @ORM\Column(name="type_service", type="string", columnDefinition="enum('Wifi', 'Parking','Espace kids','Utilites handicapes','Music','Espace fumeur','autre")")
+     */
+    private $typeService;
 
+    /**
+     * @return mixed
+     */
+    public function getTypeService()
+    {
+        return $this->typeService;
+    }
+
+    /**
+     * @param mixed $typeService
+     */
+    public function setTypeService($typeService)
+    {
+        $this->typeService = $typeService;
+    }
     /**
      * Get id
      *
