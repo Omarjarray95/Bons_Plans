d22a79ffa33c1267558332c583bc158a86317452
box\omar <omar.jarray@esprit.tn>
2018-02-13 22:55:47 +0100

    etablissement

---
+++ src/MainBundle/Form/EtablissementType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EtablissementType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('adresse')
+            ->add('description')
+            ->add('horaire')
+            ->add('services');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MainBundle\Entity\Etablissement'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mainbundle_etablissement';
+    }
+
+
+}
